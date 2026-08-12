aad02344eab466efd81c6ae26064a0b4199d3be1
Sindhu1236 <jayajayalakshmi047@gmail.com>
2026-08-12 09:36:37 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,8 @@
+name=input("enter the name")
+pas=input("enter the password")
+a=input("enter the  name")
+b=input("enter the password")
+if name==a and pas==b:
+    print("login successfully")
+else:
+    print("login failed")    
